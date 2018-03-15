bfa75e223a5b140301813a71cba5baf10cf8b7c5
rjung00712 <richard.y.jung@gmail.com>
2018-03-15 16:22:03 -0700

    added checkboxes

--- Tour Form Card/ViewController.h
+++ Tour Form Card/ViewController.h
@@ -9,8 +9,20 @@
 #import <UIKit/UIKit.h>
 #import <MessageUI/MessageUI.h>
 
-@interface ViewController : UIViewController<MFMailComposeViewControllerDelegate>
+@interface ViewController : UIViewController<MFMailComposeViewControllerDelegate> {
+    BOOL checked0;
+    BOOL checked1;
+    BOOL checked2;
+    BOOL checked3;
+    NSString *buttonImage;
+}
 
+
+@property (weak, nonatomic) IBOutlet UIButton *checkButton_0;
+@property (weak, nonatomic) IBOutlet UIButton *checkButton_1;
+@property (weak, nonatomic) IBOutlet UIButton *checkButton_2;
+@property (weak, nonatomic) IBOutlet UIButton *checkButton_3;
+- (IBAction)clickedCheckButton:(id)sender;
 
 @end
 
--- Tour Form Card/ViewController.m
+++ Tour Form Card/ViewController.m
@@ -18,6 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.viewToBeConverted setAutoresizesSubviews:YES];
+    
+    checked0 = NO;
+    checked1 = NO;
+    checked2 = NO;
+    checked3 = NO;
+    buttonImage = @"Black_check.png";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,4 +50,82 @@
     [self presentViewController:vc animated:YES completion:nil];
 }
 
+- (IBAction)clickedCheckButton:(id)sender {
+    UIButton *button = (UIButton *) sender;
+    
+    switch (button.tag) {
+        case 0:
+            if (!checked0) {
+                [self.checkButton_0 setImage:[UIImage imageNamed:buttonImage] forState:UIControlStateNormal];
+                checked0 = YES;
+            } else {
+                [self.checkButton_0 setImage:nil forState:UIControlStateNormal];
+                checked0 = NO;
+            }
+            break;
+        case 1:
+            if (!checked1) {
+                [self.checkButton_1 setImage:[UIImage imageNamed:buttonImage] forState:UIControlStateNormal];
+                checked1 = YES;
+            } else {
+                [self.checkButton_1 setImage:nil forState:UIControlStateNormal];
+                checked1 = NO;
+            }
+            break;
+        case 2:
+            if (!checked2) {
+                [self.checkButton_2 setImage:[UIImage imageNamed:buttonImage] forState:UIControlStateNormal];
+                checked2 = YES;
+            } else {
+                [self.checkButton_2 setImage:nil forState:UIControlStateNormal];
+                checked2 = NO;
+            }
+            break;
+        case 3:
+            if (!checked3) {
+                [self.checkButton_3 setImage:[UIImage imageNamed:buttonImage] forState:UIControlStateNormal];
+                checked3 = YES;
+            } else {
+                [self.checkButton_3 setImage:nil forState:UIControlStateNormal];
+                checked3 = NO;
+            }
+            break;
+        default:
+            break;
+    }
+    
+    
+//    if (button.tag == 0) {
+//        if (!checked0) {
+//            [self.checkButton_0 setImage:[UIImage imageNamed:buttonImage] forState:UIControlStateNormal];
+//            checked0 = YES;
+//        } else {
+//            [self.checkButton_0 setImage:nil forState:UIControlStateNormal];
+//            checked0 = NO;
+//        }
+//    } else if (button.tag == 1) {
+//        if (!checked1) {
+//            [self.checkButton_1 setImage:[UIImage imageNamed:buttonImage] forState:UIControlStateNormal];
+//            checked1 = YES;
+//        } else {
+//            [self.checkButton_1 setImage:nil forState:UIControlStateNormal];
+//            checked = NO;
+//
+//            NSLog(@"1 un-clicked");
+//        }
+//
+//    }
+    
+//    if (!checked) {
+//        [self.checkButton_0 setImage:[UIImage imageNamed:@"Black_check.png"] forState:UIControlStateNormal];
+//        checked = YES;
+//
+//        NSLog(@"clicked");
+//    } else {
+//        [self.checkButton_0 setImage:nil forState:UIControlStateNormal];
+//        checked = NO;
+//
+//        NSLog(@"un-clicked");
+//    }
+}
 @end
